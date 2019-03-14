6d55e951246fac16a00639b461e95d2a1d0fc58f
Adam Niedzwiecki <adamn8142@gmail.com>
2019-03-14 12:07:29 -0600

    Mess

--- src/Splash.js
+++ src/Splash.js
@@ -2,31 +2,31 @@ import React, { Component } from 'react';
 import "./Splash.css"
 
 class Splash extends Component {
-  
+
   handleClick = (event) => {
     event.preventDefault();
     this.props.enterApp();
   }
 
   render() {
-    return(
+    return (
       <div>
         <button className="enter-button" onClick={this.handleClick}>ENTER</button>
         <div className="fade">
           <div className="star-wars">
-            <div className ="crawl">
-              <div className = "name-and-date">
+            <div className="crawl">
+              <div className="name-and-date">
                 <h1>{this.props.filmScroll.title}</h1>
                 <p>{this.props.filmScroll.release_date}</p>
               </div>
               <p>{this.props.filmScroll.opening_crawl}</p>
             </div>
-          </div> 
+          </div>
         </div>
       </div>
     )
   }
 }
 
-export default Splash; 
+export default Splash;
 
